541d987378f203f2e939b1e00925e0b75cc887de
ovshake <abhishek16005@iiitd.ac.in>
2020-05-10 11:53:21 +0530

    remove redundant lines

--- HW-2/question2.py
+++ HW-2/question2.py
@@ -41,22 +41,22 @@ def get_mfcc(sample, samplerate, plotpath, window = 20, overlap = .5):
 	if samplerate in fbank_dict:
 		filter_banks = fbank_dict[samplerate]
 	else:
-		high_freq_mel = (2595 * np.log10(1 + (samplerate / 2) / 700))  # Convert Hz to Mel
-		mel_points = np.linspace(low_freq_mel, high_freq_mel, nfilt + 2)  # Equally spaced in Mel scale
-		hz_points = (700 * (10**(mel_points / 2595) - 1))  # Convert Mel to Hz
+		high_freq_mel = (2595 * np.log10(1 + (samplerate / 2) / 700)) 
+		mel_points = np.linspace(low_freq_mel, high_freq_mel, nfilt + 2)
+		hz_points = (700 * (10**(mel_points / 2595) - 1)) 
 		bin = np.floor((NFFT + 1) * hz_points / samplerate)
 		fbank = np.zeros((nfilt, int(np.floor(NFFT / 2 + 1))))
 		for m in range(1, nfilt + 1):
-			f_m_minus = int(bin[m - 1])   # left
-			f_m = int(bin[m])             # center
-			f_m_plus = int(bin[m + 1])    # right
+			f_m_minus = int(bin[m - 1])   
+			f_m = int(bin[m])             
+			f_m_plus = int(bin[m + 1])    
 			for k in range(f_m_minus, f_m):
 				fbank[m - 1, k] = (k - bin[m - 1]) / (bin[m] - bin[m - 1])
 			for k in range(f_m, f_m_plus):
 				fbank[m - 1, k] = (bin[m + 1] - k) / (bin[m + 1] - bin[m])
 		filter_banks = np.dot(windows, fbank.T)
-		filter_banks = np.where(filter_banks == 0, np.finfo(float).eps, filter_banks)  # Numerical Stability
-		filter_banks = 20 * np.log10(filter_banks)  # dB
+		filter_banks = np.where(filter_banks == 0, np.finfo(float).eps, filter_banks) 
+		filter_banks = 20 * np.log10(filter_banks)
 		fbank_dict[samplerate] = filter_banks
 
 	filter_banks = fbank_dict[samplerate]
